eecd5b48c1cfb25a05beef5f882c06569e6f18a8
Andrew Stacy <ajstacy@gmail.com>
2022-11-15 12:00:05 -0600

    Wrote unit tests for isString, hasOwnProperty, and hasOwnProperties.

---
+++ test/hasOwnProperties.test.ts
@@ -0,0 +1,18 @@
+import { hasOwnProperties } from '../src';
+
+const testObj = {
+  foo: "bar",
+  hello: "world",
+};
+
+describe('hasOwnProperties type guard', () => {
+  it('validates that an object contains all of the provided properties', () => {
+    expect(hasOwnProperties(testObj, ['foo', 'hello'])).toBe(true);
+  });
+  it('validates that an object must contain all of the provided properties', () => {
+    expect(hasOwnProperties(testObj, ['foo', 'baz'])).toBe(false);
+  });
+  it('validates that an object does not contain the provided properties', () => {
+    expect(hasOwnProperties(testObj, ['scooby', 'doo'])).toBe(false);
+  });
+});
---
+++ test/hasOwnProperty.test.ts
@@ -0,0 +1,15 @@
+import { hasOwnProperty } from '../src';
+
+const testObj = {
+  foo: "bar",
+  hello: "world",
+};
+
+describe('hasOwnProperties type guard', () => {
+  it('validates that an object contains the provided property', () => {
+    expect(hasOwnProperty(testObj, 'foo')).toBe(true);
+  });
+  it('validates that an object does not contain the provided property', () => {
+    expect(hasOwnProperty(testObj, 'baz')).toBe(false);
+  });
+});
