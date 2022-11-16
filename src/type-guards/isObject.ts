b73f3c38dd3960ca274bfe47edfc9a1e988b6615
Andrew Stacy <ajstacy@gmail.com>
2022-11-16 10:49:47 -0600

    Fixed TS error with isObject. Using Yarn instead of npm now.

--- src/type-guards/isObject.ts
+++ src/type-guards/isObject.ts
@@ -1,8 +1,9 @@
-import { isSet } from "./isSet";
-
 /**
  * Type guard that validates that a value is an Object.
  */
 export function isObject(val?: unknown): val is Record<string, unknown> {
-  return isSet<unknown>(val) && typeof val === 'object' && val.constructor === Object;
+  return val !== undefined &&
+    val !== null &&
+    typeof val === 'object' &&
+    val?.constructor === Object;
 }
