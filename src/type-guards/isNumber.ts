58c3c45c1ba13174566ff5b1e43be22800beb6cd
Andrew Stacy <ajstacy@gmail.com>
2023-10-17 09:32:58 -0400

    Updated isNumber TypeGuard for Accuracy with null values
    isNumber() now checks for null values because Number(null) returns 0 instead of NaN.

--- src/type-guards/isNumber.ts
+++ src/type-guards/isNumber.ts
@@ -2,5 +2,6 @@
  * Type Guard to validate that the value is a number.
  */
 export function isNumber(value: unknown): value is number {
-  return !isNaN(Number(value));
+  // Number(null) returns 0 😭
+  return value !== null && !isNaN(Number(value));
 }
